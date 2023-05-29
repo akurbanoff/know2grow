1708a8b5e6449d0a60864d6f2d1ed4488fe928e9
neekee <Uspeshniy.ae@yandex.ru>
2023-05-29 12:41:02 +0300

    create connection to binance api

--- src/main.py
+++ src/main.py
@@ -1,12 +1,13 @@
+import os
 import time
-
 import uvicorn
+import asyncio
 from fastapi import FastAPI, Depends, UploadFile, File
 from fastapi_users import InvalidPasswordException
 from src.auth.routers import router as auth_router
 from src.auth.auth import auth_backend
 from src.auth.schemas import UserRead, UserUpdate, UserCreate
-from src.config import *
+from src.config import CRYPTO_PANIC_API, BINANCE_API, BINANCE_SECRET_KEY
 #from src.auth.models import google_oauth_client, oauth_scheme
 from src.auth.manager import UserManager
 from src.utils import get_user_db
@@ -23,12 +24,60 @@ from redis import asyncio as aioredis
 from fastapi_cache import FastAPICache
 from fastapi_cache.backends.redis import RedisBackend
 from fastapi_cache.decorator import cache
+from binance import AsyncClient
+from binance.streams import BinanceSocketManager
 
 
 app = FastAPI(
     title='know2grow',
     version='0.0.1'
 )
+
+
+async def create_binance_client():
+    binance_client = await AsyncClient.create(api_key=BINANCE_API, api_secret=BINANCE_SECRET_KEY)
+    return binance_client
+
+
+async def create_binance_manager(binance_client= Depends(create_binance_client)):
+    binance_manager = BinanceSocketManager(binance_client, user_timeout=60)
+
+    return binance_manager
+
+
+async def process_message(msg):
+    return msg
+
+
+@app.get('/socket_binance')
+async def socket_binance(currency: str, binance_manager: BinanceSocketManager = Depends(create_binance_manager),
+                         binance_client: AsyncClient = Depends(create_binance_client)):
+    '''
+    Сокет соединение с бинансом. Длится 60 секунд, но походу бесконечно.
+    - currency: принимает валюту по которой запрашиваются данные в формате тикера - BTCUSDT, ETHUSDT
+
+    Возвращает данные в формате:
+    {
+      "e": "trade", - тип события, который указывает на тип транзакции.
+      "E": 1685352493842, -  время события в миллисекундах.
+      "s": "BTCUSDT", - символ пары торгов
+      "t": 3128786077, - ID сделки.
+      "p": "27892.87000000", - цена по которой произошла сделка.
+      "q": "0.00181000", - количество базовой валюты, которое было продано или куплено.
+      "b": 21277176651, - ID покупателя.
+      "a": 21277175869, - ID продавца.
+      "T": 1685352493842, - время выполнения операции в миллисекундах.
+      "m": false, - флаг режима: true - если сделка происходит за пределами стакана (вне очереди), и false - если сделка происходит внутри стакана.
+      "M": true - флаг maker: true - если покупатель является создателем спроса (maker), и false - если покупатель является исполнителем заявки (taker).
+    }
+    '''
+    ts = binance_manager.trade_socket(symbol=currency)
+    async with ts as tscm:
+        while True:
+            res = await tscm.recv()
+            return res
+
+    await binance_client.close_connection()
 
 
 @app.on_event('startup')
@@ -71,9 +120,20 @@ current_user = auth_router.current_user()
 # )
 
 @app.post('/auth/change_password')
-async def change_password(email: str, new_password: str, test_db: bool = False):
+async def change_password(email: str, new_password: str):
+    '''
+    Смена пароля с хэшированием.
+
+    Параметры:
+    - email(str): ввод почты, по которой будет браться юзер и меняться пароль
+    - new_password(str): длина больше 8 без @ или эл почты в пароле, также должен быть хоть 1 символ в верхнем регистре.
+
+    Return:
+    - 200 OK
+    '''
+
     rounds = 12
-    salt = b'$2b$12$'#.......................'
+    salt = b'$2b$12$.......................'
 
     if len(new_password) < 8:
         raise InvalidPasswordException(
@@ -91,11 +151,10 @@ async def change_password(email: str, new_password: str, test_db: bool = False):
     new_password = new_password.encode('utf-8')
     password = bcrypt.hashpw(password=new_password, salt=salt)
 
-    if not test_db:
-        async with session() as conn:
-            stmt = update(User).values(hashed_password=password.decode('utf-8')).where(User.email == email)
-            await conn.execute(stmt)
-            await conn.commit()
+    async with session() as conn:
+        stmt = update(User).values(hashed_password=password.decode('utf-8')).where(User.email == email)
+        await conn.execute(stmt)
+        await conn.commit()
 
     return JSONResponse(content={
         'result': 'OK'
@@ -105,6 +164,19 @@ async def change_password(email: str, new_password: str, test_db: bool = False):
 @app.get('/get_crypto_news')
 async def get_crypto_news(get_all: bool = False, currency: str = None, get_by_tag: str = None,
                           get_from_to: datetime.datetime = None, user=Depends(current_user)):
+    '''
+    Получает новости про криптовалюту и эту сферу в целом.
+
+    Параметры:
+    - get_all (bool): если значение True, то будут получены 100 последних новостей.
+    - currency (str): поиск по типу валюты. Вводите в формате BTC, ETH и тд.
+    - get_by_tag (str): ключевое слово для поиска.
+    - get_from_to (datatime): даты публикации новостей с какого-то числа и по какое-то число.
+
+    Примечание:
+    - Для использования некоторых параметров требуется авторизация пользователя.
+    - Функция возвращает список новостей.
+    '''
     url = f'https://cryptopanic.com/api/v1/posts/?auth_token={CRYPTO_PANIC_API}'
     response = None
 
