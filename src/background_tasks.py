04c741b4a8b1738af995e87b9b016dfcc253d874
Uspesh <Uspeshniy.ae@yandex.ru>
2023-06-15 16:49:51 +0300

    change: message in end-point send_hello_email_message

--- src/background_tasks.py
+++ src/background_tasks.py
@@ -3,6 +3,7 @@ from email.message import EmailMessage
 
 from celery import Celery
 from src.config import SMTP_HOST, SMTP_PORT, SMTP_USER, KNOW2GROW_SMTP_PASS
+from src.crypto_news.routers import current_user
 
 celery = Celery('tasks', broker='redis://localhost:6379')
 
@@ -11,9 +12,11 @@ def send_hello_email_message(username: str):
     email = EmailMessage()
     email['Subject'] = 'know2grow'
     email['From'] = SMTP_USER
-    email['To'] = 'uspeshniy.ae@yandex.ru'
+    email['To'] = current_user.email
 
-    email.set_content(f'Привет {username}! Мы рады, что ты к нам присоединился. Вперед к мечте и не скучай!')
+    email.set_content(f'''Привет {username}! Мы рады, что ты к нам присоединился. Чтобы увидеть пример использования сайта,
+        перейдите по ссылке http://127.0.0.1:8000/example .
+    ''')
 
     return email
 
