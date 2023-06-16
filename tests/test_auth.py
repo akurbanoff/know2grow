cf4c9cd9b4db46bc391a741a63eb6994b4e6f36a
Uspesh <Uspeshniy.ae@yandex.ru>
2023-06-16 15:19:15 +0300

    add: 2 tests, which test change name and email

--- tests/test_auth.py
+++ tests/test_auth.py
@@ -29,6 +29,28 @@ def test_login():
     assert response.status_code == 204
 
 
+def test_change_name():
+    data = {
+        "name": "Artem",
+        "email": "sir.re-zero@yandex.ru",
+        'new_name': 'Alex'
+    }
+    response = client.post(url='/auth/change_name', params=data)
+
+    assert response.status_code == 200
+
+
+def test_change_email():
+    data = {
+        "name": "Alex",
+        "email": "sir.re-zero@yandex.ru",
+        'new_email': 'sir.re-zero@yandex.ru'
+    }
+    response = client.post(url='/auth/change_email')
+
+    assert response.status_code == 200
+
+
 def test_change_password():
     data = {
         'email': 'sir.re-zero@yandex.ru',
@@ -39,18 +61,3 @@ def test_change_password():
     response = client.post(url='/auth/change_password', params=data)
 
     assert response.status_code == 200
-
-
-def test_login_with_changed_params():
-    data = {
-        'grant_type': '',
-        'username': 'sir.re-zero@yandex.ru',
-        'password': 'EnCoDe88',
-        'scope': '',
-        'client_id': '',
-        'client_secret': ''
-    }
-
-    response = client.post(url='/auth/jwt/login', data=data)
-
-    assert response.status_code == 400
